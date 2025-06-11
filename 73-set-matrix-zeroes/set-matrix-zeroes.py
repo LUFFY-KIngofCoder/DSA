3d1e75404eefb5f4ee7da97ea8e2905e21112c1d
LUFFY-KIngofCoder <65397647+LUFFY-KIngofCoder@users.noreply.github.com>
2025-06-11 10:19:09 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (77.38%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -3,32 +3,21 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        n = len(matrix)
-        m = len(matrix[0])
-        x = []
+        m = len(matrix)
+        n = len(matrix[0])
+        x = set() 
         y = set()
 
-        for i in range(n):
+        for i in range(m):
             if 0 in matrix[i]:
-                # start = 0
-                x.append(i)
-                # while True:
-                #     try:
-                #         yi = matrix[i].index(0 , start)
-                #         start = yi+1
-                #         y.add(yi)
-                        
-                #     except:
-                #         break  
-                for j in range(len(matrix[i])):
-                    if matrix[i][j] == 0:
+                x.add(i)
+                for j in range(n):
+                    if 0 == matrix[i][j]:
                         y.add(j)
-
-
-        for j in x:
-            for i in range(m):
-                matrix[j][i] = 0
+        for i in x:
+            for j in range(n):
+                matrix[i][j] = 0
         
         for j in y:
-            for i in range(n):
+            for i in range(m):
                 matrix[i][j] = 0
