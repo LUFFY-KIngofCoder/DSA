3ba0107a94726e1f8c5b87ad2849f87445fd3655
LUFFY-KIngofCoder <65397647+LUFFY-KIngofCoder@users.noreply.github.com>
2025-06-12 19:01:23 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (56.98%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -5,20 +5,26 @@ class Solution:
         """
         a = None
         n = len(nums)
-        for i in range(n-1):
-            if nums[i+1] > nums[i]:
-                a = i
+        for i in range(n-1,0,-1):
+            if nums[i] > nums[i-1]:
+                a = i-1
+                break
         
         if a == None:
             nums.sort()
             return
 
-        m = None
-        for i in range(a+1,n):
-            if (m == None) and (nums[i] > nums[a]):
-                m = i
-            elif nums[i] > nums[a] and nums[i] < nums[m]:
-                    m = i
-        
-        nums[m] , nums[a] = nums[a] , nums[m]
+        #m = None
+        # for i in range(a+1,n):
+        #     if (m == None) and (nums[i] > nums[a]):
+        #         m = i
+        #     elif nums[i] > nums[a] and nums[i] < nums[m]:
+        #             m = i
+        # nums[m] , nums[a] = nums[a] , nums[m]
+
+        for j in range(n-1, a,-1):
+            if nums[j] > nums[a]:
+                nums[j] , nums[a] = nums[a] , nums[j]
+                break
+
         nums[a+1:] = sorted(nums[a+1:])
