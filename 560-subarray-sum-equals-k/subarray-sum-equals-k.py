4c70d4d8edf88fb75eaa36e30a9d5c3354feb841
LUFFY-KIngofCoder <65397647+LUFFY-KIngofCoder@users.noreply.github.com>
2025-06-14 14:18:35 +0530

    Time: 35 ms (49.80%) | Memory: 22 MB (19.38%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,22 +1,39 @@
+from collections import defaultdict
+
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        has = {}
-        s = 0
-        n = 0
+        # has = {}
+        # s = 0
+        # n = 0
 
+        # for i in nums:
+           
+        #     s += i
+
+        #     if s == k:
+        #         n+=1
+            
+        #     res = s - k
+
+        #     if res in has:
+        #         n+= has[res]
+
+        #     if s not in has:
+        #         has[s] = 1
+        #     else:
+        #         has[s] +=1
+
+        # return n
+
+        h = defaultdict(int)
+        s = 0
+        h[0] = 1
+        cnt = 0
         for i in nums:
             s += i
-            if s == k:
-                n+=1
-            
-            res = s - k
-            print(s, res)
-            if res in has:
-                n+= has[res]
+            cnt += h[s - k]
+            h[s] +=1
+        return cnt
 
-            if s not in has:
-                has[s] = 1
-            else:
-                has[s] +=1
-        print(has)
-        return n
+
+
