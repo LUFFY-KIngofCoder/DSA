9a4b8337b6e0da7c3487c119fd7c8198429d62ad
LUFFY-KIngofCoder <65397647+LUFFY-KIngofCoder@users.noreply.github.com>
2025-06-15 18:41:15 +0530

    Time: 10 ms (28.66%) | Memory: 22.4 MB (25.90%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,33 +1,42 @@
+from collections import Counter
+
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        # h = {}
-        # n = len(nums)
-        # m = n/3
-        # ans = []
+        h = {}
+        n = len(nums)
+        m = n/3
+        ans = []
 
-        # for i in nums:
-        #     if i in h:
-        #         h[i] +=1
-        #     else:
-        #         h[i] = 1
+        for i in nums:
+            if i in h:
+                h[i] +=1
+            else:
+                h[i] = 1
+            if h[i] > m and i not in ans:
+                ans.append(i)
+
+        # or 
+
+        # h = Counter(nums)
     
         # for i in h:
         #     if h[i] > m:
         #         ans.append(i)
+#---------------------------------------------------------------------------
 
-        nums.sort()
-        i = 0
-        j = 0
-        n = len(nums)
-        ans = []
-        while(j < n):
-            if nums[i] != nums[j]:
-                if j-i > n/3:
-                    ans.append(nums[i])
-                i = j
-            j+=1
-        if j-i > n/3:
-            ans.append(nums[i])
+        # nums.sort()
+        # i = 0
+        # j = 0
+        # n = len(nums)
+        # ans = []
+        # while(j < n):
+        #     if nums[i] != nums[j]:
+        #         if j-i > n/3:
+        #             ans.append(nums[i])
+        #         i = j
+        #     j+=1
+        # if j-i > n/3:
+        #     ans.append(nums[i])
 
         return ans
     
