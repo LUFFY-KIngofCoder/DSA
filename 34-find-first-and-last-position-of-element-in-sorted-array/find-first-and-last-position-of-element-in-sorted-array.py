996996e6f7ded98e18800eb9f396e937244a88a4
LUFFY-KIngofCoder <65397647+LUFFY-KIngofCoder@users.noreply.github.com>
2025-07-15 23:03:39 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (20.66%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -2,28 +2,47 @@ class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         ans = [-1,-1]
         n=len(nums)
+        # l=0
+        # r=n-1
+        # while l<=r:
+        #     mid = (l+r)//2
+        #     if nums[mid] == target:
+        #         ans[1] = mid
+        #         l = mid+1 
+        #     elif nums[mid] > target:
+        #         r= mid-1
+        #     else:
+        #         l= mid+1
+
+        # l=0
+        # r=n-1
+        # while l<=r:
+        #     mid = (l+r)//2
+        #     if nums[mid] == target:
+        #         ans[0] = mid
+        #         r = mid-1
+        #     elif nums[mid] > target:
+        #         r= mid-1
+        #     else:
+        #         l= mid+1
+#------------------------------------------------------
         l=0
         r=n-1
         while l<=r:
             mid = (l+r)//2
             if nums[mid] == target:
-                ans[1] = mid
-                l = mid+1 
+                l = mid
+                r = mid
+                while l>=0 and nums[l] == target:
+                    l-=1
+                while r<len(nums) and nums[r] == target:
+                    r+=1
+                ans = [l+1,r-1]
+                break
             elif nums[mid] > target:
                 r= mid-1
             else:
                 l= mid+1
 
-        l=0
-        r=n-1
-        while l<=r:
-            mid = (l+r)//2
-            if nums[mid] == target:
-                ans[0] = mid
-                r = mid-1
-            elif nums[mid] > target:
-                r= mid-1
-            else:
-                l= mid+1
 
         return ans
