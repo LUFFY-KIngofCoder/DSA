8148488d018e1c8182487acd0d6b2c8e043d7c75
LUFFY-KIngofCoder <65397647+LUFFY-KIngofCoder@users.noreply.github.com>
2025-07-16 00:23:05 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (66.81%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -13,11 +13,14 @@ class Solution:
                     l = mid+1
                 else:
                     r= mid-1
-            else:
+            elif nums[l] < nums[mid]:
                 if nums[l] <= target and nums[mid] >= target:
                     r = mid-1
                 else:
                     l= mid+1
+            else:
+                print(l,r,mid)
+                l= mid+1
 
         return -1
         
