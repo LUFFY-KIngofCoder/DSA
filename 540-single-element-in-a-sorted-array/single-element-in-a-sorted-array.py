0fdf356b424ae2f6234eba3b5e07f9c061c19b7b
LUFFY-KIngofCoder <65397647+LUFFY-KIngofCoder@users.noreply.github.com>
2025-07-16 23:08:41 +0530

    Time: 0 ms (100.00%) | Memory: 24.9 MB (44.55%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -22,4 +22,31 @@ def find(nums,l,r):
 
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        return find(nums,0,len(nums)-1)
+        # return find(nums,0,len(nums)-1)
+        if len(nums) == 1:
+            return nums[0]
+        l = 0
+        r = len(nums)-1
+        while l <= r:
+            mid = (l+r)//2
+            if mid != 0 and mid != len(nums)-1:
+                if nums[mid+1] != nums[mid] and nums[mid-1] != nums[mid]:
+                    return nums[mid]
+                else:
+                    if mid %2==0:
+                        if nums[mid-1] == nums[mid]:
+                            r = mid-1
+                        else:
+                            l = mid+1
+                    else:
+                        if nums[mid+1] == nums[mid]:
+                            r = mid-1
+                        else:
+                            l = mid+1   
+            elif mid == 0:
+                if nums[mid+1] != nums[mid]:
+                    return nums[mid]
+            elif mid == len(nums)-1:
+                if nums[mid-1] != nums[mid]:
+                    return nums[mid]
+
