1174946975db0bec2564816ecb8aefb0527f0722
LUFFY-KIngofCoder <65397647+LUFFY-KIngofCoder@users.noreply.github.com>
2025-07-16 20:17:04 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (50.45%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -6,10 +6,6 @@ class Solution:
         # ans = nums[0]
         # mid=((l+r)//2)
         # while l<r:
-        #     # ans = nums[mid]
-        #     # print(ans,l,r)
-        #     # if l == r:
-        #     #     break
         #     if nums[l] > nums[mid]:
         #         r = mid
         #     else:
@@ -21,16 +17,23 @@ class Solution:
         #     mid=((l+r)//2)
         # return nums[mid]
 #-------------------------------------------
-        ans = nums[0]
-        while l<=r:
-            mid = (l+r)//2
-            if nums[l] > nums[mid]:
-                r = mid-1
-                ans = min(ans , nums[mid])
+        # ans = nums[0]
+        # while l<=r:
+        #     mid = (l+r)//2
+        #     if nums[l] > nums[mid]:
+        #         r = mid-1
+        #         ans = min(ans , nums[mid])
 
-            else:
-                ans = min(ans,nums[l])
-                l= mid+1
+        #     else:
+        #         ans = min(ans,nums[l])
+        #         l= mid+1
                 
-        return ans
-        
+        # return ans
+#-------------------------------
+        while l<r:
+            mid = (l+r)//2
+            if nums[mid] > nums[r]:
+                l = mid+1
+            else:
+                r = mid
+        return nums[l]
