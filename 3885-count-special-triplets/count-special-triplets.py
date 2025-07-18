2ccd6e8b16149c74c5a87703a997bb1a99a7d534
LUFFY-KIngofCoder <65397647+LUFFY-KIngofCoder@users.noreply.github.com>
2025-07-18 18:46:27 +0530

    Time: 838 ms (37.08%) | Memory: 45.9 MB (25.88%) - LeetSync

---
+++ 3885-count-special-triplets/count-special-triplets.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+
+class Solution:
+    def specialTriplets(self, nums: List[int]) -> int:
+        count=0
+        MOD = 10**9+7
+        right = defaultdict(int)
+        for num in nums:
+            right[num]+=1
+        
+        left = defaultdict(int)
+
+        for num in nums:
+            
+            right[num]-=1
+            
+            val = num*2
+            
+            count += (right[val]%MOD) * (left[val]%MOD)
+            count %=MOD
+
+            left[num]+=1
+        
+        return count
+
+
+        return count 
+
+            
