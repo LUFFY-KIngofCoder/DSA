09ee8d0843844b956c16c79c30987231c25c152f
LUFFY-KIngofCoder <65397647+LUFFY-KIngofCoder@users.noreply.github.com>
2025-07-19 13:40:07 +0530

    Time: 239 ms (98.80%) | Memory: 31.6 MB (31.75%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.py
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.py
@@ -1,4 +1,4 @@
-def no_bouque(nums,d,k): # better
+def no_bouque(nums,d,k,m): # better
     cnt = 0
     c = 0
     for i in nums:
@@ -8,10 +8,10 @@ def no_bouque(nums,d,k): # better
             cnt+= c//k
             c = 0
     cnt+= c//k
-    return cnt
+    return cnt >=m
 
 
-def no_bouque1(nums, d,k): #okay
+def no_bouque1(nums, d,k,m): #okay
     count = 0
     c=0
     for i in nums:
@@ -23,73 +23,44 @@ def no_bouque1(nums, d,k): #okay
             count+=1
             c=0
 
-    return count
+    return count>=m
 
 class Solution:
     def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
-#         if len(bloomDay) < m*k:
-#             return -1
-        
-#         l= min(bloomDay)
-#         r = max(bloomDay)
-#         flow = m*k
-
-#         while l<r:
-#             mid = (l+r)//2
-#             print(l , r, mid, no_bouque1(bloomDay , mid,k))
-#             if no_bouque(bloomDay , mid,k) >= m:
-#                 r = mid
-#             else:
-#                 l=mid+1
-
-#         return l
-# #--------------------------------------------
-#         # val = m * k
-#         # n = len(bloomDay)  # size of the array
-#         # if val > n:
-#         #     return -1  # impossible case
-#         # # find maximum and minimum
-#         # mini = float('inf')
-#         # maxi = float('-inf')
-#         # for i in range(n):
-#         #     mini = min(mini, bloomDay[i])
-#         #     maxi = max(maxi, bloomDay[i])
-
-#         # # apply binary search
-#         # low = mini
-#         # high = maxi
-#         # while low <= high:
-#         #     mid = (low + high) // 2
-#         #     if possible(bloomDay, mid, m, k):
-#         #         high = mid - 1
-#         #     else:
-#         #         low = mid + 1
-#         # return low
-        if len(bloomDay) < m * k:
+        if len(bloomDay) < m*k:
             return -1
-        high = max(bloomDay)
-        low = min(bloomDay)
-        ans = high
-        def poss(nums, k, m, day):
-            cnt = 0
-            nobloom = 0
-            for i in range(len(nums)):
-                if nums[i] <= day:
-                    cnt += 1
-                else:
-                    nobloom += (cnt//k)
-                    cnt = 0
-            nobloom += (cnt//k)
-            if nobloom >= m:
-                return True
-            else:
-                return False     
+        
+        l= min(bloomDay)
+        r = max(bloomDay)
+        flow = m*k
 
-        while low<=high:
-            mid = (low+high) // 2
-            if poss(bloomDay, k, m, mid):
-                ans = min(ans, mid)
-                high = mid - 1
+        while l<r:
+            mid = (l+r)//2
+            if no_bouque(bloomDay , mid,k,m):
+                r = mid
             else:
-                low = mid + 1
-        return ans
+                l=mid+1
+
+        return l
+#--------------------------------------------
+        # val = m * k
+        # n = len(bloomDay)  # size of the array
+        # if val > n:
+        #     return -1  # impossible case
+        # # find maximum and minimum
+        # mini = float('inf')
+        # maxi = float('-inf')
+        # for i in range(n):
+        #     mini = min(mini, bloomDay[i])
+        #     maxi = max(maxi, bloomDay[i])
+
+        # # apply binary search
+        # low = mini
+        # high = maxi
+        # while low <= high:
+        #     mid = (low + high) // 2
+        #     if possible(bloomDay, mid, m, k):
+        #         high = mid - 1
+        #     else:
+        #         low = mid + 1
+        # return low
