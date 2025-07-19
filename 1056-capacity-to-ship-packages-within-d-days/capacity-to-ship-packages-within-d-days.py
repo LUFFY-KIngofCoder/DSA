69ad3f194e514f3a6bb462c451362a131f5006af
LUFFY-KIngofCoder <65397647+LUFFY-KIngofCoder@users.noreply.github.com>
2025-07-19 16:51:45 +0530

    Time: 182 ms (72.69%) | Memory: 22.2 MB (55.26%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,35 @@
+def result(nums, m, days):
+    s = 0
+    d = 0
+    for i in range(len(nums)):
+        s += nums[i]
+        if s == m:
+            d+=1
+            s=0
+        elif s > m:
+            d+=1
+            s=nums[i]
+         
+    d += 1 if s != 0 else 0 
+    print(d)
+    return d <= days
+
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        
+        r = sum(weights)
+        if days == 1:
+            return r
+
+        l = max(weights)
+
+        while l<r:
+            mid = (l+r)//2    
+            print(mid,l,r)
+            if result(weights,mid,days):
+                r=mid
+            else:
+                l=mid+1
+            
+        
+        return l
