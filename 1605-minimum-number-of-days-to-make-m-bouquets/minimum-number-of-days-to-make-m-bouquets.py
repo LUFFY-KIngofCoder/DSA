abe7ebe0c8cf8e978551b1cb03a7e21dbb47857b
LUFFY-KIngofCoder <65397647+LUFFY-KIngofCoder@users.noreply.github.com>
2025-07-19 12:46:05 +0530

    Time: 851 ms (5.05%) | Memory: 31.5 MB (73.55%) - LeetSync

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.py
@@ -0,0 +1,33 @@
+def no_bouque(nums, d,k):
+    count = 0
+    c=0
+    for i in nums:
+        if i - d <= 0:
+            c+=1
+        else: 
+            c=0
+        if c == k:
+            count+=1
+            c=0
+
+    return count
+
+
+class Solution:
+    def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
+        if len(bloomDay) < m*k:
+            return -1
+        
+        l= 1
+        r = max(bloomDay)
+        flow = m*k
+
+        while l<r:
+            mid = (l+r)//2
+            print(l , r, mid, no_bouque(bloomDay , mid,k))
+            if no_bouque(bloomDay , mid,k) >= m:
+                r = mid
+            else:
+                l=mid+1
+
+        return l
