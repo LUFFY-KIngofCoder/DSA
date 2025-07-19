65fd77f232d1238605a6c999973e815a841add87
LUFFY-KIngofCoder <65397647+LUFFY-KIngofCoder@users.noreply.github.com>
2025-07-19 16:55:52 +0530

    Time: 206 ms (32.88%) | Memory: 22.1 MB (55.26%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -1,16 +1,14 @@
 def result(nums, m, days):
     s = 0
-    d = 0
+    d = 1
     for i in range(len(nums)):
-        s += nums[i]
-        if s == m:
-            d+=1
-            s=0
-        elif s > m:
+        if s + nums[i] > m:
             d+=1
             s=nums[i]
+        else:
+            s+=nums[i]
          
-    d += 1 if s != 0 else 0 
+    # d += 1 if s != 0 else 0 
     print(d)
     return d <= days
 
