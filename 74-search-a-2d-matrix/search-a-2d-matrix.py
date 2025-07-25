06731c725e59323347984ae42f79f88f660c0f37
LUFFY-KIngofCoder <65397647+LUFFY-KIngofCoder@users.noreply.github.com>
2025-07-25 23:42:48 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (25.78%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -2,32 +2,45 @@ class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         n = len(matrix)
         m = len(matrix[0])
-        l = 0
-        r = n-1
-        print(m,n)
-        while l<=r:
-            mid = (l+r)//2
-            if matrix[mid][0] > target:
-                r = mid-1
-            elif matrix[mid][0] < target:
-                l = mid+1
-            else:
-                r = mid
-                break
+    #     l = 0
+    #     r = n-1
+    #     print(m,n)
+    #     while l<=r:
+    #         mid = (l+r)//2
+    #         if matrix[mid][0] > target:
+    #             r = mid-1
+    #         elif matrix[mid][0] < target:
+    #             l = mid+1
+    #         else:
+    #             r = mid
+    #             break
             
-        i = r
-        print(l,r) 
+    #     i = r
+    #     print(l,r) 
 
-        l = 0 
-        r = m-1
+    #     l = 0 
+    #     r = m-1
 
+    #     while l<=r:
+    #         mid = (l+r)//2
+    #         if matrix[i][mid] > target:
+    #             r = mid -1
+    #         elif matrix[i][mid] < target:
+    #             l=mid+1
+    #         else:
+    #             return True
+#--------------------------------------------------
+        l= 0
+        r = m*n-1
         while l<=r:
             mid = (l+r)//2
-            if matrix[i][mid] > target:
-                r = mid -1
-            elif matrix[i][mid] < target:
+            row = mid//m
+            col = mid%m
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] < target:
                 l=mid+1
             else:
-                return True
-          
+                r = mid-1
+
         return False           
