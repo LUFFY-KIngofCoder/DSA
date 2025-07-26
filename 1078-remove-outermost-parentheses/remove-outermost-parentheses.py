7f7eca1da9459383a4ad6fd373f05662fcfc5a9c
LUFFY-KIngofCoder <65397647+LUFFY-KIngofCoder@users.noreply.github.com>
2025-07-26 17:28:23 +0530

    Time: 3 ms (70.34%) | Memory: 18 MB (38.73%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.py
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.py
@@ -2,14 +2,22 @@ class Solution:
     def removeOuterParentheses(self, s: str) -> str:
         new = ""
         cnt = 0
+        # for i in s:
+        #     if i == "(":
+        #         cnt+=1
+        #         if cnt > 1:
+        #             new = new + i
+        #     elif i== ")":
+        #         cnt-=1
+        #         if cnt > 0:
+        #             new = new + i
         for i in s:
-            if i == "(":
-                cnt+=1
-                if cnt > 1:
-                    new = new + i
-            elif i== ")":
-                cnt-=1
-                if cnt > 0:
-                    new = new + i
+            if i == "(" and cnt>0: new=new+i
+            elif i == ")" and cnt>1: new=new+i
+
+            cnt+= 1 if i=="(" else -1
+        return new 
+
+
 
         return new
