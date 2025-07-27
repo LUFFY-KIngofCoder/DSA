886c3718ea5eb4fa07e345f6cd6d1fa2420112f0
LUFFY-KIngofCoder <65397647+LUFFY-KIngofCoder@users.noreply.github.com>
2025-07-27 23:59:06 +0530

    Time: 2949 ms (6.30%) | Memory: 17.9 MB (64.30%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,23 +1,30 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         # return Counter(s) == Counter(t)
+#--------------------------------------------------------
         n = len(s)
         m = len(t)
         if n != m:
             return False
-        a = {}
-        b = {}
-        for i in range(n):
-            if s[i] not in a:
-                a[s[i]] = 1
-            else:
-                a[s[i]] +=1 
+        # a = {}
+        # b = {}
+        # for i in range(n):
+        #     if s[i] not in a:
+        #         a[s[i]] = 1
+        #     else:
+        #         a[s[i]] +=1 
             
-            if t[i] not in b:
-                b[t[i]] = 1
-            else:
-                b[t[i]] +=1
-        for i in a:
-            if (i not in b) or (a[i] != b[i]):
+        #     if t[i] not in b:
+        #         b[t[i]] = 1
+        #     else:
+        #         b[t[i]] +=1
+        # for i in a:
+        #     if (i not in b) or (a[i] != b[i]):
+        #         return False
+        # return True
+#--------------------------------------------------------
+        ss = set(s)
+        for i in s:
+            if s.count(i) != t.count(i):
                 return False
         return True
