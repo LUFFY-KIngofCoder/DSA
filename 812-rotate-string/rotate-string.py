d4e845551ef974de7d770a574c8d692931cd9731
LUFFY-KIngofCoder <65397647+LUFFY-KIngofCoder@users.noreply.github.com>
2025-07-27 19:08:44 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (80.85%) - LeetSync

---
+++ 812-rotate-string/rotate-string.py
@@ -0,0 +1,18 @@
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        if len(s) != len(goal):
+            return False
+        n= len(s)
+        start = goal[0]
+        k = 0
+        while k < n:
+            try:    
+                k = s.index(start,k)
+                if s[k:]+s[:k] == goal:
+                    return True
+                k+=1
+            except:
+                break
+        return False 
+        
+        
