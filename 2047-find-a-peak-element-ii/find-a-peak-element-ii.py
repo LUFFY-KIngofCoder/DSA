4b7c0bf9c2d3400d59c243c920f6c196d8291c98
LUFFY-KIngofCoder <65397647+LUFFY-KIngofCoder@users.noreply.github.com>
2025-07-30 19:49:35 +0530

    Time: 0 ms (100.00%) | Memory: 46.1 MB (22.18%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.py
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.py
@@ -9,47 +9,47 @@ class Solution:
     def findPeakGrid(self, mat: List[List[int]]) -> List[int]:
         n = len(mat)
         m = len(mat[0])
-        # r = 1
-        # col = m-2
-        # l = 0
-        # r = m-1
-        # i = 0
-        # while i < n: 
-        #     print('a->',[i,l,r])
-        #     while l<=r:
-        #         mid = (l+r)//2
-        #         print('b->',[i,l,r])
-        #         if mid!=m-1 and mat[i][mid] < mat[i][mid+1]:
-        #             if l!=r:
-        #                 l=mid+1
-        #             else:
-        #                 r = mid+1
-        #         elif mid!= 0 and mat[i][mid] < mat[i][mid-1]:
-        #             if l!=r:
-        #                 r=mid-1
-        #             else:
-        #                 l=mid-1
-        #         elif i!= n-1 and mat[i][mid] < mat[i+1][mid]:
-        #             i+=1
-        #             break
-        #         elif i!= 0 and mat[i][mid] < mat[i-1][mid]:
-        #             i-=1
-        #             break
-        #         else:
-        #             return [i,mid]
+        r = 1
+        col = m-2
+        l = 0
+        r = m-1
+        i = 0
+        while i < n: 
+            print('a->',[i,l,r])
+            while l<=r:
+                mid = (l+r)//2
+                print('b->',[i,l,r])
+                if mid!=m-1 and mat[i][mid] < mat[i][mid+1]:
+                    if l!=r:
+                        l=mid+1
+                    else:
+                        r = mid+1
+                elif mid!= 0 and mat[i][mid] < mat[i][mid-1]:
+                    if l!=r:
+                        r=mid-1
+                    else:
+                        l=mid-1
+                elif i!= n-1 and mat[i][mid] < mat[i+1][mid]:
+                    i+=1
+                    break
+                elif i!= 0 and mat[i][mid] < mat[i-1][mid]:
+                    i-=1
+                    break
+                else:
+                    return [i,mid]
             
 #------------------------------
-        l = 0
-        r = m - 1
+        # l = 0
+        # r = m - 1
 
-        while l <= r:
-            mid = (l+r)//2
-            print(l,r,mid)
-            idx = max_ele(mat,mid,n)
+        # while l <= r:
+        #     mid = (l+r)//2
+        #     print(l,r,mid)
+        #     idx = max_ele(mat,mid,n)
 
-            if mid < m-1 and mat[idx][mid+1] > mat[idx][mid]:
-                l=mid+1
-            elif mid > 0 and mat[idx][mid-1] > mat[idx][mid]:
-                r=mid-1
-            else:
-                return [idx, mid]  
+        #     if mid < m-1 and mat[idx][mid+1] > mat[idx][mid]:
+        #         l=mid+1
+        #     elif mid > 0 and mat[idx][mid-1] > mat[idx][mid]:
+        #         r=mid-1
+        #     else:
+        #         return [idx, mid]  
