70f4bb13642867c747c077e1323fd1a947a94e7a
LUFFY-KIngofCoder <65397647+LUFFY-KIngofCoder@users.noreply.github.com>
2025-08-01 21:40:27 +0530

    Time: 9288 ms (5.01%) | Memory: 17.8 MB (83.60%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,19 +1,30 @@
 class Solution:
+    def isPal(self, s):
+            return s == s[::-1]
     def longestPalindrome(self, s: str) -> str:
-        if s == s[::-1]:
-            return s
-        n=len(s)
-        f = {}
-        ans = s[0]
-        for i in range(n):
-           
-            if s[i] in f.keys():
-                for j in f[s[i]]:  
-                    r = s[j:i+1]
-                    if r == r[::-1] and len(r) > len(ans):
-                        ans = r
-                f[s[i]].append(i)
-            else:
-                f[s[i]] = [i]
+        # if s == s[::-1]:
+        #     return s
+        # n=len(s)
+        # f = {}
+        # ans = s[0]
+        # for i in range(n): 
+        #     if s[i] in f.keys():
+        #         for j in f[s[i]]:  
+        #             r = s[j:i+1]
+        #             if r == r[::-1] and len(r) > len(ans):
+        #                 ans = r
+        #         f[s[i]].append(i)
+        #     else:
+        #         f[s[i]] = [i]
             
+        # return ans
+
+        
+
+        ans = ""
+        for i in range(len(s)):
+            for j in range(i, len(s)):
+                if self.isPal(s[i:j+1]):
+                    if len(ans) < len(s[i:j+1]):
+                        ans = s[i:j+1]
         return ans
