d0f9c217081a541b3ed1bb6deddd9eb137e89405
LUFFY-KIngofCoder <65397647+LUFFY-KIngofCoder@users.noreply.github.com>
2025-08-12 23:37:08 +0530

    Time: 46 ms (23.52%) | Memory: 18.7 MB (18.39%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -9,19 +9,44 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        h = {}
-        dummy = Node(0)
-        new_head = dummy
-        org_start = head
-        while head: 
-            new_head.next = Node(head.val)
-            new_head = new_head.next
-            h[head] = new_head
-            head = head.next
-        new_head = dummy.next
+        # h = {}
+        # dummy = Node(0)
+        # new_head = dummy
+        # org_start = head
+        # while head: 
+        #     new_head.next = Node(head.val)
+        #     new_head = new_head.next
+        #     h[head] = new_head
+        #     head = head.next
+        # new_head = dummy.next
 
-        for i in h:
-            if i.random:
-                h[i].random = h[i.random]
+        # for i in h:
+        #     if i.random:
+        #         h[i].random = h[i.random]
         
-        return new_head
+        # return new_head
+
+        start = head
+        while head:
+            a = Node(head.val)
+            a.next = head.next
+            head.next = a
+
+            head = head.next.next
+        head = start 
+
+        while head:
+            head.next.random = head.random.next if head.random else None
+            head = head.next.next
+
+        head = start
+        dummy = Node(-1)
+        start = dummy
+
+        while head:
+            start.next = head.next
+            head.next = head.next.next
+            head = head.next
+            start = start.next
+        
+        return dummy.next
