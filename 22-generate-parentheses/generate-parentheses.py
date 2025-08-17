15e5c9b9a1fb0211671c43d070659c183a37a6f4
LUFFY-KIngofCoder <65397647+LUFFY-KIngofCoder@users.noreply.github.com>
2025-08-17 17:20:54 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (69.36%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,16 +1,16 @@
-def answer(s , ans, n,count):
+def answer(s , ans, n,f,b):
     if len(s)== n*2:
         ans.append(s)
         return 
-    if s.count("(")!= n:    
-        answer(s+"(" , ans, n, count+1)
-    if s != "" and count != 0:
-        answer(s+")" , ans, n, count-1)
+    if f!= n:    
+        answer(s+"(" , ans, n, f+1,b)
+    if f!= 0 and f!= b :
+        answer(s+")" , ans, n, f,b+1)
 
     
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         s = ""
         ans = []
-        answer(s,ans,n,0)
+        answer(s,ans,n,0,0)
         return ans
