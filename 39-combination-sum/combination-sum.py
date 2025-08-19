96978872df7126e8cdae14a59eb176b69b2a822c
LUFFY-KIngofCoder <65397647+LUFFY-KIngofCoder@users.noreply.github.com>
2025-08-19 14:36:34 +0530

    Time: 9 ms (65.13%) | Memory: 18.1 MB (19.69%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -3,16 +3,31 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         ans = []
         ds = []
-        def answer(i,s):
-            # nonlocal ans ,ds
-            if s > target or i == len(candidates):
+        # def answer(i,s):
+        #     # nonlocal ans ,ds
+        #     if s > target or i == len(candidates):
+        #         return
+        #     if s == target:
+        #         ans.append(ds.copy())
+        #         return
+        #     ds.append(candidates[i])
+        #     answer(i,s+candidates[i])
+        #     ds.pop()
+        #     answer(i+1,s)
+        # answer(0,0)
+
+#--------------------------------------------------------
+        def answer(i, target):
+            if i == len(candidates):
+                if target == 0:
+                    ans.append(ds.copy())
+                    return
                 return
-            if s == target:
-                ans.append(ds.copy())
-                return
-            ds.append(candidates[i])
-            answer(i,s+candidates[i])
-            ds.pop()
-            answer(i+1,s)
-        answer(0,0)
+            if candidates[i] <= target:
+                ds.append(candidates[i])
+                answer(i , target - candidates[i])
+                ds.pop()
+            answer(i+1 , target)
+        answer(0,target)
+
         return ans
