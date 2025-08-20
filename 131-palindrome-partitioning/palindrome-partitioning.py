df009b724f73da4456097489cce4d36b4fd0f146
LUFFY-KIngofCoder <65397647+LUFFY-KIngofCoder@users.noreply.github.com>
2025-08-20 22:17:31 +0530

    Time: 59 ms (24.26%) | Memory: 34.5 MB (45.44%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -0,0 +1,20 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        ans = []
+        ds = []
+
+        def answer(i):
+            if i == len(s):
+                if ds!= []:
+                    ans.append(ds.copy())
+                return
+            for j in range(i,len(s)):
+                if s[i:j+1] == s[i:j+1][::-1]:
+                    ds.append(s[i:j+1])
+                    answer(j+1)
+                    ds.pop()
+                
+
+        answer(0)
+        return ans
+                
