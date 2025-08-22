9a23642f10b6d1b47aabc2d9addb99c2e35f0062
LUFFY-KIngofCoder <65397647+LUFFY-KIngofCoder@users.noreply.github.com>
2025-08-22 22:45:28 +0530

    Time: 2393 ms (91.81%) | Memory: 17.9 MB (39.69%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,39 +1,41 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         
-        def answer(i , j ,s , h):  
+        def answer(i , j ,s):  
             if s == word:
                 return True
-            
-            if i!=0 and ((i-1,j) not in h) and word[len(s)] == board[i-1][j]: 
-                top = answer(i-1,j,s+board[i-1][j], h+[(i-1,j)])
+
+            temp, board[i][j] = board[i][j], "#"
+
+            if i!=0 and word[len(s)] == board[i-1][j]: 
+                top = answer(i-1,j,s+board[i-1][j])
                 if top:
                     return True
             
             
-            if j!= len(board[0])-1 and ((i,j+1) not in h) and word[len(s)] == board[i][j+1]:
-                right = answer(i,j+1,s+board[i][j+1],h+[(i,j+1)])
+            if j!= len(board[0])-1 and word[len(s)] == board[i][j+1]:
+                right = answer(i,j+1,s+board[i][j+1])
                 if right:
                     return True
         
             
-            if j!= 0 and ((i,j-1) not in h) and word[len(s)] == board[i][j-1]:
-                left = answer(i,j-1,s+board[i][j-1],h+[(i,j-1)])
+            if j!= 0 and word[len(s)] == board[i][j-1]:
+                left = answer(i,j-1,s+board[i][j-1])
                 if left:
                     return True
             
             
-            if i!= len(board)-1 and ((i+1,j) not in h) and word[len(s)] == board[i+1][j]:
-                bottom = answer(i+1,j,s+board[i+1][j],h+[(i+1,j)])
+            if i!= len(board)-1 and word[len(s)] == board[i+1][j]:
+                bottom = answer(i+1,j,s+board[i+1][j])
                 if bottom:
                     return True
-
+            board[i][j] = temp
             return False
         
         for i in range(len(board)):
             for j in range(len(board[0])):
                 if board[i][j] == word[0]:
-                    ans = answer(i,j,word[0],[(i,j)])
+                    ans = answer(i,j,word[0])
                     if ans:
                         return True
         return False
