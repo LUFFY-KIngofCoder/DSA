a3707ef864140e6f70c076cfc389ce950a02b403
LUFFY-KIngofCoder <65397647+LUFFY-KIngofCoder@users.noreply.github.com>
2025-08-22 22:38:12 +0530

    Time: 5740 ms (15.40%) | Memory: 17.9 MB (39.69%) - LeetSync

---
+++ 79-word-search/word-search.py
@@ -0,0 +1,39 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        
+        def answer(i , j ,s , h):  
+            if s == word:
+                return True
+            
+            if i!=0 and ((i-1,j) not in h) and word[len(s)] == board[i-1][j]: 
+                top = answer(i-1,j,s+board[i-1][j], h+[(i-1,j)])
+                if top:
+                    return True
+            
+            
+            if j!= len(board[0])-1 and ((i,j+1) not in h) and word[len(s)] == board[i][j+1]:
+                right = answer(i,j+1,s+board[i][j+1],h+[(i,j+1)])
+                if right:
+                    return True
+        
+            
+            if j!= 0 and ((i,j-1) not in h) and word[len(s)] == board[i][j-1]:
+                left = answer(i,j-1,s+board[i][j-1],h+[(i,j-1)])
+                if left:
+                    return True
+            
+            
+            if i!= len(board)-1 and ((i+1,j) not in h) and word[len(s)] == board[i+1][j]:
+                bottom = answer(i+1,j,s+board[i+1][j],h+[(i+1,j)])
+                if bottom:
+                    return True
+
+            return False
+        
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == word[0]:
+                    ans = answer(i,j,word[0],[(i,j)])
+                    if ans:
+                        return True
+        return False
