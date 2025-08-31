4999cb5b89ba66ff056afc2887005c4238dd5774
LUFFY-KIngofCoder <65397647+LUFFY-KIngofCoder@users.noreply.github.com>
2025-08-31 22:13:40 +0530

    Time: 112 ms (10.12%) | Memory: 19.5 MB (41.88%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.py
@@ -0,0 +1,47 @@
+class Stack:
+    def __init__(self):
+        self.Top = -1
+        self.size = 10**4
+        self.arr = [0]*self.size
+
+    def push(self, x):
+        self.Top+=1
+        self.arr[self.Top] = x
+        
+    def pop(self) -> str:
+        if self.isEmpty():
+            raise IndexError("Stack is empty")
+        x = self.arr[self.Top]
+        self.Top-=1
+        return x
+
+    def top(self) -> str:
+        if self.isEmpty():
+            raise IndexError("Stack is empty")
+        return self.arr[self.Top]
+
+    def isEmpty(self):
+        return self.Top == -1
+
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        st = Stack()
+        n = len(nums)
+        nge = [0]*n
+        ind = nums.index(max(nums))
+        print(ind)
+        for i in range(n+ind,-1,-1):
+            i = i%n
+            while not st.isEmpty() and st.top() <= nums[i]:
+                st.pop()
+            print(st.isEmpty())
+            if st.isEmpty():
+                nge[i] = -1
+            else:
+                nge[i] = st.top()
+                # print(st.top())
+            st.push(nums[i])
+            # print(nge)
+
+        return nge
+
